3eb6620790e0cca7c5da692665019dc214fdb6a3
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2025-03-10 16:18:36 +0100

    :fix: fix routes, foreach to show diy articles

--- modules/backoffice/views/BackCreateEditArticleView.php
+++ modules/backoffice/views/BackCreateEditArticleView.php
@@ -21,8 +21,7 @@ class BackCreateEditArticleView extends View
     {
         $isEditing = $this->article !== null;
         ob_start();
-        var_dump($_SESSION);
-        var_dump($_COOKIE);
+
 ?>
         <div class="container mx-auto p-4">
             <h1 class="text-2xl font-bold mb-4">
