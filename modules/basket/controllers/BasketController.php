483b9904a8942a354ed63052df47ef833195de83
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2025-03-10 22:51:33 +0100

    :sparkles: + and - int the basket

--- modules/basket/controllers/BasketController.php
+++ modules/basket/controllers/BasketController.php
@@ -4,6 +4,9 @@ class BasketController
 {
     public function execute()
     {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         $view = new BasketView();
         $view->show();
     }
@@ -14,7 +17,6 @@ class BasketController
             session_start();
         }
 
-        var_dump($_POST);
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             for ($i = 0; $i < count($_POST['productId']); $i++) {
                 $productId = $_POST['productId'][$i];
@@ -24,7 +26,7 @@ class BasketController
                 if (isset($_SESSION['cart'][$productId])) {
                     $_SESSION['cart'][$productId]['quantity'] += $quantity;
                 } else {
-                    $_SESSION['cart'][$product] = [
+                    $_SESSION['cart'][$productId] = [
                         'productId' => $productId,
                         'product' => $product,
                         'price' => $price,
@@ -32,13 +34,82 @@ class BasketController
                     ];
                 }
             }
-            /* $_SESSION = []; */
             $view = new BasketView();
             $view->show();
         } else {
             $view = new page404View();
             $view->show();
         }
+    }
+
+    public function addOneToCart()
+    {
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $jsonData = json_decode(file_get_contents('php://input'), true);
+
+        $product = $jsonData['product'] ?? null;
+        $productId = $jsonData['productId'] ?? null;
+        $quantity = $jsonData['quantity'] ?? null;
+        $price = $jsonData['price'] ?? 0;
+
+        if (isset($_SESSION['cart'][$productId])) {
+            $_SESSION['cart'][$productId]['quantity'] = $quantity; // Ou += 1 pour incrémenter
+        } else {
+            $_SESSION['cart'][$productId] = [
+                'productId' => $productId,
+                'product' => $product,
+                'price' => $price,
+                'quantity' => $quantity
+            ];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode([
+            'success' => true,
+            'session' => $_SESSION,
+            'product' => $product,
+            'quantity' => $quantity
+        ]);
+        exit;
+    }
+
+    public function removeOneToCart()
+    {
+
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        $jsonData = json_decode(file_get_contents('php://input'), true);
+
+        $product = $jsonData['product'] ?? null;
+        $productId = $jsonData['productId'] ?? null;
+        $quantity = $jsonData['quantity'] ?? null;
+        $price = $jsonData['price'] ?? 0;
+
+        if (isset($_SESSION['cart'][$productId])) {
+            $_SESSION['cart'][$productId]['quantity'] = $quantity; // Ou += 1 pour incrémenter
+        } else {
+            $_SESSION['cart'][$productId] = [
+                'productId' => $productId,
+                'product' => $product,
+                'price' => $price,
+                'quantity' => $quantity
+            ];
+        }
+
+        header('Content-Type: application/json');
+        echo json_encode([
+            'success' => true,
+            'session' => $_SESSION,
+            'product' => $product,
+            'quantity' => $quantity
+        ]);
+        exit;
     }
 
     public function testPanier()
